91dc25d82e7422c32e7fa0f8c9e4944cfb00b5dd
Rudolf <rudolphn34@gmail.com>
2019-04-05 21:48:55 +0100

    Initial commit

---
+++ src/main/java/com/example/BookingSoftware/BookingSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookingSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSoftwareApplication.class, args);
+	}
+
+}
